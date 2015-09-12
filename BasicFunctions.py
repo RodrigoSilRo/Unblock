d463a3c721b10a3aacc7f44989f99d50a2c04d39
RodrigoSilRo <rodrigosilveiradarosa@gmail.com>
2015-09-12 02:50:02 -0400

    Update BasicFunctions.py

--- BasicFunctions.py
+++ BasicFunctions.py
@@ -1,13 +1,20 @@
+##Naming Vehicles: C=car; T=Truck; V=Vertical; H=Horizontal; M=Main Car; 0=Free space
+##e.g. HC3 stands for third horizontal car from up to down
+##e.g. VT1 stands for first vertical car from left to right
+
 def createemptymatrix():
+#creates a 6x6 zero-filled matrix
 	matrix = [[0 for x in range(6)]for x in range(6)]
 	return matrix
 	
 def up(matrix, car):
 	for i in range(6):
 		for j in range(6):
+			#searching the field for a specific car
 			if matrix[i][j] == car:
-				if (i-1>-1):
-					if (matrix[i-1][j] == 0):
+				if (i-1>-1): #if it won´t get out of the boundary:
+					if (matrix[i-1][j] == 0): #if it won´t hit another vehicle:
+						#move up!
 						matrix[i-1][j] = car;
 						matrix[i][j] = car;
 						matrix[i+1][j] = 0;
@@ -63,10 +70,12 @@ def right(matrix, car):
 					return "Error"
 					
 def printmatrix(matrix):
+#just prints the matrix as a table
 	for i in range(6):
 		print matrix[i]
 		
 def createfield():
+#this is our pre-defined field (game level)
 	matrix=createemptymatrix()
 	matrix[2][2]="v1"
 	matrix[3][2]="v1"
@@ -80,6 +89,7 @@ def createfield():
 	return matrix
 		
 def evaluate(matrix):
+#to win the game, the main car (M) should get to 6th row from the 3rd line (exit of the parking lot)
 	if matrix[2][5] == "M":
 		return "WON"
 	else:
